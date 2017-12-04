3f8b61c2a6c55c311f62776b1a682e6cf105df11
Peter Hewitt <peter.hewitt@cardinalhealth.com>
2017-12-04 13:01:56 -0500

    run in landscape and add preview windows of filtering

--- app/src/main/java/com/cardinalhealth/pillcounter/ColorBlobDetectionActivity.java
+++ app/src/main/java/com/cardinalhealth/pillcounter/ColorBlobDetectionActivity.java
@@ -1,54 +1,44 @@
 package com.cardinalhealth.pillcounter;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
-import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
 import org.opencv.core.MatOfPoint;
+import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ColorBlobDetectionActivity extends Activity implements View.OnTouchListener, CameraBridgeViewBase.CvCameraViewListener2 {
-    private static final String  TAG              = "OCVSample::Activity";
+    private static final String TAG = "OCVSample::Activity";
 
     private Mat mRgba;
     private CameraBridgeViewBase mOpenCvCameraView;
-
-    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
-        @Override
-        public void onManagerConnected(int status) {
-            switch (status) {
-                case LoaderCallbackInterface.SUCCESS:
-                {
-                    mOpenCvCameraView.enableView();
-                    //mOpenCvCameraView.setOnTouchListener(ColorBlobDetectionActivity.this);
-                } break;
-                default:
-                {
-                    super.onManagerConnected(status);
-                } break;
-            }
-        }
-    };
 
     public ColorBlobDetectionActivity() {
         Log.i(TAG, "Instantiated new " + this.getClass());
     }
 
-    /** Called when the activity is first created. */
+    /**
+     * Called when the activity is first created.
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         Log.i(TAG, "called onCreate");
@@ -58,24 +48,22 @@ public class ColorBlobDetectionActivity extends Activity implements View.OnTouch
 
         setContentView(R.layout.color_blob_detection_surface_view);
 
-        mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.color_blob_detection_activity_surface_view);
+        mOpenCvCameraView = findViewById(R.id.color_blob_detection_activity_surface_view);
         mOpenCvCameraView.setCvCameraViewListener(this);
     }
 
     @Override
-    public void onPause()
-    {
+    public void onPause() {
         super.onPause();
         if (mOpenCvCameraView != null)
             mOpenCvCameraView.disableView();
     }
 
     @Override
-    public void onResume()
-    {
+    public void onResume() {
         super.onResume();
         if (OpenCVLoader.initDebug()) {
-            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+            mOpenCvCameraView.enableView();
         }
     }
 
@@ -108,11 +96,11 @@ public class ColorBlobDetectionActivity extends Activity implements View.OnTouch
         return mRgba;
     }
 
-    public void updateCountTextOnScreen(Integer currentCount){
+    public void updateCountTextOnScreen(Integer currentCount) {
         final String count = currentCount.toString();
-        final TextView textView = (TextView) findViewById(R.id.countText);
+        final TextView textView = findViewById(R.id.countText);
 
-        (this).runOnUiThread(new Runnable() {
+        runOnUiThread(new Runnable() {
             @Override
             public void run() {
                 textView.setText("Current Count: " + count);
@@ -122,14 +110,18 @@ public class ColorBlobDetectionActivity extends Activity implements View.OnTouch
 
     private int processImage(Mat imageBmp) {
         try {
-            Mat threeChannel = new Mat();
+            Mat gray = new Mat();
+            Imgproc.cvtColor(imageBmp, gray, Imgproc.COLOR_BGR2GRAY);
+            preview(gray, R.id.filter_image_view1);
 
-            Imgproc.cvtColor(imageBmp, threeChannel, Imgproc.COLOR_BGR2GRAY);
-            Imgproc.threshold(threeChannel, threeChannel, 220, 255, Imgproc.THRESH_BINARY);
+            Mat thresh = new Mat();
+            Imgproc.threshold(gray, thresh, 220, 255, Imgproc.THRESH_BINARY);
+            preview(thresh, R.id.filter_image_view2);
 
             Mat dist = new Mat();
-            Imgproc.distanceTransform(threeChannel, dist, Imgproc.CV_DIST_L2, 3);
+            Imgproc.distanceTransform(gray, dist, Imgproc.CV_DIST_L2, 3);
             Imgproc.threshold(dist, dist, 0.5, 1.0, Imgproc.THRESH_BINARY);
+
             Mat dist8u = new Mat();
             dist.convertTo(dist8u, CvType.CV_8U);
 
@@ -139,20 +131,27 @@ public class ColorBlobDetectionActivity extends Activity implements View.OnTouch
             Imgproc.findContours(dist8u, contours, hierarchy, Imgproc.RETR_EXTERNAL,
                 Imgproc.CHAIN_APPROX_SIMPLE);
 
-            final int currentCount = contours.size();
-            final TextView textView = (TextView) findViewById(R.id.countText);
+            return contours.size();
 
-            (this).runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    textView.setText("Current Count: " + currentCount);
-                }
-            });
-
-            return currentCount;
         } catch (Exception e) {
             Log.e(TAG, "Error processing pill image", e);
             return 0;
         }
+    }
+
+    private void preview(Mat mat, final int imageViewId) {
+        MatOfByte matOfByte = new MatOfByte();
+        Imgcodecs.imencode(".jpg", mat, matOfByte);
+        byte[] data = matOfByte.toArray();
+        Bitmap bmp = BitmapFactory.decodeByteArray(data, 0, data.length);
+        final Bitmap mutableBitmap = bmp.copy(Bitmap.Config.ARGB_8888, true);
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                ImageView imageView = findViewById(imageViewId);
+                imageView.setImageBitmap(mutableBitmap);
+            }
+        });
     }
 }
